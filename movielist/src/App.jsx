3c6f58d617e67e2fdd46bb8e068a9e6cbf56e440
fako4 <fako4545@hotmail.com.tr>
2024-03-24 01:13:09 +0300

    add: Header component and css codes

--- movielist/src/App.jsx
+++ movielist/src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import Header from './components/Header'
 
 function App() {
  
 
   return (
     
-      <div> hello world</div>
+     <div className="App"><Header/></div>
        
 
   )
---
+++ movielist/src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+const Header = () => {
+  return (
+    <header>
+        <div className="container">
+            <div className="inner-content">
+                <div className="brand">
+                    İzlenecekler
+                </div>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
