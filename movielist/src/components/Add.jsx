d7090bc149998813c9e7824f3cd8480129edb691
fako4 <fako4545@hotmail.com.tr>
2024-03-24 19:16:17 +0300

    Add:fetch themoviedb api and .env file

--- movielist/src/components/Add.jsx
+++ movielist/src/components/Add.jsx
@@ -3,12 +3,24 @@ import { useState } from "react"
 
 const Add = () => {
     const[query, setQuery] = useState("");
+    const [results, setResults] = useState([]);
+    
     
     function onChange (e) {
         setQuery(e.target.value)
-
-    }
-
+//fetch themoviedb api
+fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      if (!data.errors) {
+        setResults(data.results);
+      } else {
+        setResults([]);
+      }
+    });
+}
   return (
     <div className="add-page">
         <div className="container">
@@ -17,13 +29,17 @@ const Add = () => {
             <div className="titles">
                 <h1>Hoş Geldiniz.</h1>
                 <h2>Milyonlarca film, TV şovu ve keşfedilecek kişi. Şimdi keşfedin.</h2>
-                {query}
+                
                 </div>
                 <div className="input-wrapper">
             <input type="text" 
+            value={query}
             onChange={onChange}
             placeholder="Film, dizi, kişi ara ..."/>
             </div>
+            {results.map((movie) =>(
+                <h1 key={movie.id}>{movie.title}</h1>
+            ))}
             </div>
             
         </div>
