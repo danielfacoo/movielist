534419e84e538a5049dae0cd978c7abdd8750603
fako4 <fako4545@hotmail.com.tr>
2024-03-24 16:26:06 +0300

    Add: router and component of watchlist

--- movielist/src/App.jsx
+++ movielist/src/App.jsx
@@ -1,13 +1,19 @@
-
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import './App.css'
 import Header from './components/Header'
+import Watchlist from "./components/Watchlist";
 
 function App() {
  
 
   return (
     
-     <div className="App"><Header/></div>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Watchlist/>}/>
+      </Routes>
+    </Router>
        
 
   )
